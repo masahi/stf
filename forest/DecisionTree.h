8d2b30058eb57ac6f541b939d225bce867569c4f
masahi <masahi129@gmail.com>
2013-12-14 02:27:39 +0000

    added itk

--- forest/DecisionTree.h
+++ forest/DecisionTree.h
@@ -19,19 +19,19 @@ public:
 
     DecisionTree(int n_classes_, int n_features, int n_thresholds, int max_d)
         : n_classes(n_classes_),
-          n_candidate_feat(n_features),
-          n_thres_per_feat(n_thresholds),
-          max_depth(max_d),
-          n_nodes(0)
+        n_candidate_feat(n_features),
+        n_thres_per_feat(n_thresholds),
+        max_depth(max_d),
+        n_nodes(0)
     {
 
     }
 
     template <typename FeatureContainer, typename LabelContainer>
     void train(const FeatureContainer& X,
-               const LabelContainer& y,
-               std::vector<int>& indices,
-               const std::function<FeatureType* ()>& factory)
+        const LabelContainer& y,
+        std::vector<int>& indices,
+        const std::function<FeatureType* ()>& factory)
     {
         buildTree(X, y, indices, factory);
     }
@@ -40,11 +40,11 @@ public:
     std::vector<double> predictDistribution(const D& x) const
     {
         int index = 0;
-        while(!nodes[index]->isLeaf())
+        while (!nodes[index]->isLeaf())
         {
             SplitNode<FeatureType>* node = dynamic_cast<SplitNode<FeatureType>*>(nodes[index].get());
             const double response = node->getFeatureResponse(x);
-            if(response < node->getThreshold()) index = nodes[index]->getLeftChildIndex();
+            if (response < node->getThreshold()) index = nodes[index]->getLeftChildIndex();
             else index = nodes[index]->getRightChildIndex();
         }
         return dynamic_cast<LeafNode*>(nodes[index].get())->getDistribution();
@@ -54,12 +54,12 @@ private:
 
     struct NodeBuildInfo
     {
-        NodeBuildInfo(int from_, int to_, int parent_index_, bool is_left_, int depth_):
-            from(from_),
-            to(to_),
-            parent_index(parent_index_),
-            is_left(is_left_),
-            depth(depth_)
+        NodeBuildInfo(int from_, int to_, int parent_index_, bool is_left_, int depth_) :
+        from(from_),
+        to(to_),
+        parent_index(parent_index_),
+        is_left(is_left_),
+        depth(depth_)
         {
         }
 
@@ -70,17 +70,17 @@ private:
         const int depth;
     };
 
-    template <typename FeatureContainer,typename LabelContainer>
+    template <typename FeatureContainer, typename LabelContainer>
     void buildTree(const FeatureContainer& X,
-                         const LabelContainer& y,
-                         std::vector<int>& indices,
-                         const std::function<FeatureType* ()>& factory)
+        const LabelContainer& y,
+        std::vector<int>& indices,
+        const std::function<FeatureType* ()>& factory)
     {
         std::queue<NodeBuildInfo> que;
         que.push(NodeBuildInfo(0, indices.size(), 0, false, 0));
         bool is_root = true;
 
-        while(!que.empty())
+        while (!que.empty())
         {
             NodeBuildInfo info(que.front());
             que.pop();
@@ -99,7 +99,7 @@ private:
             double best_thres;
 
             int n_threshold;
-            if(n_thres_per_feat == -1)
+            if (n_thres_per_feat == -1)
             {
                 n_threshold = n_data - 1;
             }
@@ -113,21 +113,21 @@ private:
             int prev_label = y[indices[from]];
             bool same_label = true;
 
-            for(int i = from; i < to; ++i)
+            for (int i = from; i < to; ++i)
             {
                 int l = y[indices[i]];
                 parent_hist.accumulate(l);
-                if(l != prev_label) same_label = false;
+                if (l != prev_label) same_label = false;
                 prev_label = l;
             }
 
-            if(max_depth <= depth || same_label || n_data <= 5)
+            if (max_depth <= depth || same_label || n_data <= 5)
             {
                 LeafNode* leaf = new LeafNode(node_index, depth, parent_hist);
                 nodes.push_back(NodePtr(leaf));
                 ++n_nodes;
 
-                if(is_left)
+                if (is_left)
                 {
                     nodes[parent_index]->setLeftChildIndex(node_index);
                 }
@@ -140,16 +140,16 @@ private:
             }
 
 
-            for(int i = 0; i < n_candidate_feat; ++i)
+            for (int i = 0; i < n_candidate_feat; ++i)
             {
                 std::shared_ptr<FeatureType> f(factory());
-                for(int j = from; j < to; ++j)
+                for (int j = from; j < to; ++j)
                 {
-                    response[j-from] = (*f)(X[indices[j]]);
+                    response[j - from] = (*f)(X[indices[j]]);
                 }
-                if(n_data != n_threshold)
+                if (n_data != n_threshold)
                 {
-                    for(int j = 0; j < threshold.size(); ++j)
+                    for (int j = 0; j < threshold.size(); ++j)
                     {
                         threshold[j] = response[randInt(0, n_data)];
                     }
@@ -161,20 +161,20 @@ private:
 
                 std::sort(threshold.begin(), threshold.end());
 
-                if(threshold[0] == threshold[n_threshold])
+                if (threshold[0] == threshold[n_threshold])
                 {
                     continue;
                 }
 
-                for(int j = 0; j < n_threshold; ++j)
+                for (int j = 0; j < n_threshold; ++j)
                 {
-                    threshold[j] = threshold[j] + (double)rand()/RAND_MAX * (threshold[j+1] - threshold[j]);
+                    threshold[j] = threshold[j] + (double)rand() / RAND_MAX * (threshold[j + 1] - threshold[j]);
                 }
 
                 std::vector<Histogram> partition_statistics(threshold.size(), Histogram(n_classes));
-                for(int j = from; j < to; ++j)
+                for (int j = from; j < to; ++j)
                 {
-                    int t = std::upper_bound(threshold.begin(), threshold.begin() + n_threshold, response[j-from]) - threshold.begin();
+                    int t = std::upper_bound(threshold.begin(), threshold.begin() + n_threshold, response[j - from]) - threshold.begin();
                     partition_statistics[t].accumulate(y[indices[j]]);
                 }
 
@@ -187,7 +187,7 @@ private:
                 }
 
                 double gain = computeInfomationGain(parent_hist, left_statistics, right_statistics);
-                if(gain > best_gain)
+                if (gain > best_gain)
                 {
                     best_gain = gain;
                     best_feature = f;
@@ -195,14 +195,14 @@ private:
 
                 }
 
-                for(int t = 1; t < n_threshold; ++t)
+                for (int t = 1; t < n_threshold; ++t)
                 {
                     left_statistics.accumulate(partition_statistics[t]);
                     right_statistics.decrease(partition_statistics[t]);
 
                     gain = computeInfomationGain(parent_hist, left_statistics, right_statistics);
 
-                    if(gain > best_gain)
+                    if (gain > best_gain)
                     {
                         best_gain = gain;
                         best_feature = f;
@@ -212,13 +212,13 @@ private:
                 }
             }
 
-            if(best_gain <= 0.01)
+            if (best_gain <= 0.01)
             {
                 LeafNode* leaf = new LeafNode(node_index, depth, parent_hist);
                 nodes.push_back(NodePtr(leaf));
 
                 ++n_nodes;
-                if(is_left)
+                if (is_left)
                 {
                     nodes[parent_index]->setLeftChildIndex(node_index);
                 }
@@ -230,32 +230,32 @@ private:
                 continue;
             }
 
-            for(int i = from; i < to; ++i)
+            for (int i = from; i < to; ++i)
             {
-                response[i-from] = (*best_feature)(X[indices[i]]);
+                response[i - from] = (*best_feature)(X[indices[i]]);
             }
 
             SplitNode<FeatureType>* split(new SplitNode<FeatureType>(node_index, depth, best_feature, best_thres));
             nodes.push_back(NodePtr(split));
             ++n_nodes;
 
-            if(is_root)
+            if (is_root)
             {
                 is_root = false;
             }
             else
             {
-                if(is_left)
-            {
-                nodes[parent_index]->setLeftChildIndex(node_index);
-            }
-            else
-            {
-                nodes[parent_index]->setRightChildIndex(node_index);
-            }
+                if (is_left)
+                {
+                    nodes[parent_index]->setLeftChildIndex(node_index);
+                }
+                else
+                {
+                    nodes[parent_index]->setRightChildIndex(node_index);
+                }
             }
 
-            int thres_index = partitionByResponse(indices,from, to, response, best_thres);
+            int thres_index = partitionByResponse(indices, from, to, response, best_thres);
 
             que.push(NodeBuildInfo(from, thres_index, node_index, true, depth));
             que.push(NodeBuildInfo(thres_index, to, node_index, false, depth));
